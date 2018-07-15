127bc985bcf88be4bb35970b6eee48a0bb79bda0
Ellen Lee <leechinghui.tw@gmail.com>
2018-07-15 23:54:23 +0800

    add ImgDisplay

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import ImgDisplay from '@/components/ImgDisplay'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/ImgDisplay',
+      name: 'ImgDisplay',
+      component: ImgDisplay
+    }
+  ]
+})
